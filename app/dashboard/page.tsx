54e5dcc46fede1530131ad0155c57606712cef53
Shadi <ossaili.apps@gmail.com>
2023-07-19 23:53:19 +0300

    na

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      ss
-      {/* <UserButton afterSignOutUrl="/" /> */}
-    </div>
+    <Button variant="default" size={"lg"}>
+      dashboard
+    </Button>
   );
 }
